58328b30a9af3e65017ea9502a66b518b45530a3
Ald28 <aldopichihua123@gmail.com>
2024-12-07 10:30:46 -0500

    end commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Cargar las variables de entorno
 
-// Crear la conexión a la base de datos
+// Crear la conexión a la base de datos con las variables de entorno
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',  // Cambia según tu configuración
-    password: '1234',  // Cambia según tu configuración
-    database: 'crud_personas'
+    host: process.env.DB_HOST,        // Toma del archivo .env
+    user: process.env.DB_USER,        // Toma del archivo .env
+    password: process.env.DB_PASSWORD,// Toma del archivo .env
+    database: process.env.DB_NAME     // Toma del archivo .env
 });
 
 // Verificar la conexión
